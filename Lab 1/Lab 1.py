816d36c1454241cb9ed066ab75827f1b07dabd93
LP404 <71828222+LP404@users.noreply.github.com>
2022-01-21 16:10:21 +0000

    Update Lab 1.py

--- Lab 1/Lab 1.py
+++ Lab 1/Lab 1.py
@@ -1,3 +1,19 @@
 import numpy as np
+from numpy import random as nprnd
+import random as rnd
 import scipy as sp
+import time as t
 
+SeedInt = int(t.time())
+
+nprnd.seed(SeedInt)
+rnd.seed(SeedInt)
+
+
+def Walter(TotalNum):
+    
+    RandInt = 1
+    
+    
+    
+    return RandInt
