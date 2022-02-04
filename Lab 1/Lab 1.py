9f136df940da66aa92cafde0c1e007e7aa773787
LP404 <71828222+LP404@users.noreply.github.com>
2022-02-04 15:27:11 +0000

    Multiple Changes See Description
    1) Set Seed generator to a fixed value
    2) Added a chi-squared value generator function

--- Lab 1/Lab 1.py
+++ Lab 1/Lab 1.py
@@ -4,6 +4,8 @@ import random as rnd
 import matplotlib.pyplot as plt
 from scipy.ndimage.interpolation import shift
 import time as t
+#Check time simulation
+
 
 def shiftFunc(array,shiftVal):
     
@@ -20,10 +22,32 @@ def shiftFunc(array,shiftVal):
             
         return shiftedArray
 
+def chiSQ(inputArray,binNo):
+    Observed = np.zeros(binNo)
+    Expected = len(inputArray) / binNo
+    BinBoundary = np.arange(0,1+(1/binNo),(1/binNo))
+    for i in range(0,len(Observed)):
+        for k in range(0,len(inputArray)):
+            if inputArray[k] >= BinBoundary[i] and BinBoundary[i+1] > inputArray[k] :
+                Observed[i] += 1
+            #The above if statement will not count any instance of the inputArray when it is one, this next line fizes that
+            elif  inputArray[k] == 1:
+                Observed[i] += 1   
+            else:
+               #pass is a null operation the IDE kept throwing me an error about the next line and Pass seemed to placate it
+               pass
+    ChiSq = np.sum((Observed - Expected)**2 / Expected)
+    
+    return ChiSq
+
+#Probability of exceeding chi squadred with 9 degrees of freedom and alpha = 0.05 is 16.92
 
 TotalNum = int(input("How many random numbers would you like to generate? : "))
 
-Seed = int(t.time())
+#RandomSeed = t.time()
+#Set Seed = 1337
+
+Seed = int(1337)
 
 PCG64 = np.random.Generator(np.random.PCG64(seed=np.random.SeedSequence(Seed)))
 SFC64 = np.random.Generator(np.random.SFC64(seed=np.random.SeedSequence(Seed)))
@@ -46,7 +70,10 @@ Xaxis4 = Xaxis2 - Xaxis2[int((len(Xaxis2)-1)/2)]
 
 
 
-TotalParticles = int(input("How many particles are in the box? : "))
-ParticleLocX = nprnd.rand(TotalParticles)
-ParticleLocX-=1 
-ParticleLocY = nprnd.rand(TotalParticles)
+
+
+# TotalParticles = int(input("How many particles are in the box? : "))
+# ParticleLocX = nprnd.rand(TotalParticles)
+# ParticleLocX-=1 
+# ParticleLocY = nprnd.rand(TotalParticles)
+
