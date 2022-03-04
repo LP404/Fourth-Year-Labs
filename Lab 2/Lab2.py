c6d342f258dda0583dfc87e77eb90d4ee94f4acc
LP404 <71828222+LP404@users.noreply.github.com>
2022-03-04 17:53:17 +0000

    Continued working on Lancia037
    > WalterRohl renamed Lancia037
    
    > Original Lancia037 removed to due to it being unnecessary
    > Added in random number generation for the samples
    
    > Added some test functions to try out on Lancia037 when it is completed

--- Lab 2/Lab2.py
+++ Lab 2/Lab2.py
@@ -3,14 +3,23 @@ import scipy as sp
 from inspect import signature
 import quantumrandom as qrand
 
-#Integrand
-def func():
-    return 2
-    
+#Seed = int(t.time())
+Seed = 22061910
 
-#Named after driver that won the 1983 Monte-Carlo rally 
-def WalterRohl(Integrad):
-    Dims = len(signature(func).parameters)
+SFC64 = np.random.Generator(np.random.SFC64(seed=np.random.SeedSequence(Seed)))
+
+#Generates an array of n lengths filled with random numbers from 0 to 1
+
+#Integrand
+def func(x):
+    return 4*x
+   
+def circlefunc(x,r):
+    return (r**2 - x**2)**0.5
+
+#Named after the car that won the 1983 Monte-Carlo rally
+def Lancia037(NoSamples,Integrand):
+    Dims = len(signature(Integrand).parameters)
     
     #This is for the edge case of an integrand that consists of one or more constants
     if Dims == 0:
@@ -31,17 +40,14 @@ def WalterRohl(Integrad):
         Lims[i].insert(1,lower)
     
     
-    return Dims, Lims
-
-
-#Named after the car that won the 1983 Monte-Carlo rally
-def Lancia037(Samples,Dims,Lims):
-        
-        
-        
-    return
-
-
-
-Lancia037(WalterRohl(func()))
-
+    
+    
+    samples = SFC64.uniform(Lims[0][0],Lims[0][1],NoSamples)
+                  
+    # for i in range(Dims):
+    #     sum()
+    
+    
+    
+    
+    return samples
