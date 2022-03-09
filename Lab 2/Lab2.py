0b3120b3728dc5c62e52ef86664b04aa5ed1c832
LP404 <71828222+LP404@users.noreply.github.com>
2022-03-09 23:25:28 +0000

    Reworked sanity check, started work on n-ball function

--- Lab 2/Lab2.py
+++ Lab 2/Lab2.py
@@ -6,48 +6,67 @@ import quantumrandom as qrand
 #Seed = int(t.time())
 Seed = 22061910
 
+#vars()['Force_'+str(x)]
+
 SFC64 = np.random.Generator(np.random.SFC64(seed=np.random.SeedSequence(Seed)))
 
 #Generates an array of n lengths filled with random numbers from 0 to 1
 
 #Integrand
-def func(x):
-    return 4*x
-   
-def circlefunc(x,r):
-    return (r**2 - x**2)**0.5
+
+def xy(DataIn):
+
+    x = DataIn[0]
+    y = DataIn[1]
+
+    return x + y
+
+def nDcube(DataIn):
+    return DataIn
 
 #Named after the car that won the 1983 Monte-Carlo rally
-def Lancia037(NoSamples,Integrand):
-    Dims = len(signature(Integrand).parameters)
+def Lancia037(NoSamples,Integrand,NoDims):
+    DimsCheck = len(signature(Integrand).parameters)
+    Dims = NoDims
     
-    #This is for the edge case of an integrand that consists of one or more constants
-    if Dims == 0:
-        Dims = 1
+    if DimsCheck > Dims:
+        print('There are dimentions that have no limits assigned to them')
+        return
     else:
         pass
+    
     Lims = [[] for _ in range(0,Dims)]
+    SampleBox = [[] for _ in range(0,Dims)]
     
     for i in range(Dims):
         while True:   
             try:
-                upper = float(input('Value of upper limit : '))
-                lower = float(input('Value of lower limit : '))
+                lower = float(input('Value of lower limit for dimention '+str(i+1)+' : '))
+                upper = float(input('Value of upper limit for dimention '+str(i+1)+' : '))
+                print('\n')
                 break
             except ValueError:
                 print("Value is not a real number, try again")
-        Lims[i].insert(0,upper)
-        Lims[i].insert(1,lower)
+        Lims[i].insert(0,lower)
+        Lims[i].insert(1,upper)
     
     
+    for i in range(Dims):
+        samples = SFC64.uniform(Lims[i][0],Lims[i][1],NoSamples)
+        SampleBox[i] = samples 
     
+    Ssum = Integrand(SampleBox)
     
-    samples = SFC64.uniform(Lims[0][0],Lims[0][1],NoSamples)
-                  
-    # for i in range(Dims):
-    #     sum()
+    Total = sum(Ssum)
+
+    Coef = np.prod(np.diff(Lims))
+
+    error = Coef * (np.std(Ssum) / (NoSamples)**0.5)
     
+    return Lims,SampleBox, (Coef / NoSamples) * Total, error
+Lims,SampleBox,Result,Error = Lancia037(1000000,nDcube,2)
+
+#This car has won several monte carlo rallies
+def AudiQuattroA2(Samples,Lims):
     
-    
-    
-    return samples
+    return
