57306e80a0c682ab9b6015cc25503f81c3b75d94
LP404 <71828222+LP404@users.noreply.github.com>
2022-03-18 17:32:53 +0000

    Added nBall volume calculator

--- Lab 2/Lab2.py
+++ Lab 2/Lab2.py
@@ -30,7 +30,7 @@ def nDcube(DataIn):
 #Named after the car that won the 1983 Monte-Carlo rally
 
 NoSamples = 10000
-NoDims = 2
+NoDims = 3
 
 #def Lancia037(NoSamples,Integrand,NoDims):
 DimsCheck = len(signature(Integrand).parameters)
@@ -62,24 +62,27 @@ for i in range(Dims):
 for i in range(Dims):
     samples = SFC64.uniform(Lims[i][0],Lims[i][1],NoSamples)
     SampleBox[i] = samples
-    SampleBoxSq[i] = samples**2
+    
 
+SampleBoxSq = SqList(SampleBox)
 
-Ssum = Integrand(SampleBox)
-SsumSq = Integrand(SampleBoxSq)
+# Ssum = Integrand(SampleBox)
+# SsumSq = Integrand(SampleBoxSq)
 
-Total = sum(Ssum)
-TotalSq = sum(SsumSq)
+# ExpectSsum = sum(Ssum) / NoSamples
+# ExpectSsumSq = sum(SsumSq) / NoSamples
 
-Var = ((TotalSq) - (Total)**2) / NoSamples
+#Var = ((TotalSq) - (Total)**2) / NoSamples
 
-Std = (Var)**0.5
+#Std = (Var)**0.5
 
 Coef = np.prod(np.diff(Lims))
 
-error = Coef * (np.std(Ssum) / (NoSamples)**0.5)
+Total = np.sum(Integrand(SampleBox))
 
-error2 = Coef * Std / (NoSamples)**0.5
+# error = Coef * (np.std(Ssum) / (NoSamples)**0.5)
+
+#error2 = Coef * Std / (NoSamples)**0.5
     
     #return Lims,SampleBox, (Coef / NoSamples) * Total, error,error2, Coef, Std
 
@@ -88,5 +91,40 @@ error2 = Coef * Std / (NoSamples)**0.5
 
 #This car has won several monte carlo rallies
 def AudiQuattroA2(Samples,Lims):
+    #checks if the limits pretaine to a n ball, by checking if they are all the same length in each axis and occupy the same respective coorsinates in each axis
+    check = True
+    for i in range(1,len(Lims)):
+        if Lims[i] == Lims[i-1]:
+            pass
+        else:
+            check = False
     
-    return
+    if check == False:
+        print('nBall calcualtion is not possible, check limits')
+        return
+    else:
+        pass
+    
+    #we know that the equation for an n ball is dim1**2 + dim2**2 + ... + dimN**2 = radius**2
+    #Factor in center points
+    
+    
+    rsq = (np.diff(Lims)[0][0]/2)**2
+    
+    InSphere = 0
+    
+    for i in range(0,len(Samples[0])):
+        total = 0
+        for j in range(0,len(Lims)):
+            total += ([row[i] for row in Samples][j] - np.mean(Lims[0]))**2
+        
+        
+        if total <= rsq:
+            InSphere += 1
+        else:
+            pass
+    
+    Volume = np.prod(np.diff(Lims)) * (InSphere / len(Samples[0]))
+    
+    
+    return InSphere ,Volume
